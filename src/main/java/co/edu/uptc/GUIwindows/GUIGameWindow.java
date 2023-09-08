f812130c73844d7b96f18ee9a2db57bf23a5a815
SebastianLizarazo <sdlizarazo9@misena.edu.co>
2023-09-08 08:24:26 -0500

    agregacion archivos de imagenes

--- src/main/java/co/edu/uptc/GUIwindows/GUIGameWindow.java
+++ src/main/java/co/edu/uptc/GUIwindows/GUIGameWindow.java
@@ -9,9 +9,9 @@ import java.time.LocalTime;
 public class GUIGameWindow extends JFrame {
 
     private JPanel JPMainArea;
-    private JTextField JTTxtOne;//Deben ser cambiados por los espacios para imagenes
-    private JTextField JTTxtTwo;//Deben ser cambiados por los espacios para imagenes
-    private JTextField JTTxtThree;//Deben ser cambiados por los espacios para imagenes
+    private JLabel JLImagesOne;//Deben ser cambiados por los espacios para imagenes
+    private JLabel JLImagesTwo;//Deben ser cambiados por los espacios para imagenes
+    private JLabel JLImagesThree;//Deben ser cambiados por los espacios para imagenes
 
     private JButton JBtnOne;
     private JButton JBtnTwo;
@@ -20,33 +20,41 @@ public class GUIGameWindow extends JFrame {
     public GUIGameWindow(){
         JPMainArea = new JPanel();
 
-        JTTxtOne = new JTextField();
-        JTTxtTwo = new JTextField();
-        JTTxtThree = new JTextField();
+        JLImagesOne = new JLabel();
+        JLImagesTwo = new JLabel();
+        JLImagesThree = new JLabel();
 
         JBtnOne = new JButton("Stop");
         JBtnTwo = new JButton("Stop");
         JBtnThree = new JButton("Stop");
 
         setTitle("Pantalla de juego");
-        setSize(600,300);
+        setSize(800,700);
         setLayout(new BorderLayout());
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
     }
 
     public void GameScreen(){//Es el equivalente del metodo begin del ejemplo de hilos
+        JPanel JPCenter = new JPanel();
 
-        JPMainArea.setLayout(new GridLayout(2,3));//Organiza los componentes en una cuadricula
+        JPMainArea.setBounds(0, 0, 800, 700);
+        JPMainArea.setBackground(new Color(242, 242, 242));
+
+        JPMainArea.setLayout(null);
+
+        JPCenter.setLayout(new GridLayout(2,3));//Organiza los componentes en una cuadricula
+        JPCenter.setBounds(200,200,400,300);
 
 
-        JPMainArea.add(JTTxtOne);
-        JPMainArea.add(JTTxtTwo);
-        JPMainArea.add(JTTxtThree);
-        JPMainArea.add(JBtnOne);
-        JPMainArea.add(JBtnTwo);
-        JPMainArea.add(JBtnThree);
-        this.add(JPMainArea,BorderLayout.CENTER);
+        add(JPMainArea);
+        JPCenter.add(JLImagesOne);
+        JPCenter.add(JLImagesTwo);
+        JPCenter.add(JLImagesThree);
+        JPCenter.add(JBtnOne);
+        JPCenter.add(JBtnTwo);
+        JPCenter.add(JBtnThree);
+        JPMainArea.add(JPCenter,BorderLayout.CENTER);
 
         setVisible(true);
 
@@ -56,9 +64,9 @@ public class GUIGameWindow extends JFrame {
 
     private void addComponents() {//El metodo controlador
 
-        ThreadCount runOne = new ThreadCount(JTTxtOne,true);
-        ThreadCount runTwo = new ThreadCount(JTTxtTwo,false);
-        ThreadCount runThree = new ThreadCount(JTTxtThree,true);
+        ThreadCount runOne = new ThreadCount(JLImagesOne,true);
+        ThreadCount runTwo = new ThreadCount(JLImagesTwo,false);
+        ThreadCount runThree = new ThreadCount(JLImagesThree,true);
 
         Thread thOne = new Thread(runOne);
         Thread thTwo = new Thread(runTwo);
--- src/main/java/co/edu/uptc/logic/ThreadCount.java
+++ src/main/java/co/edu/uptc/logic/ThreadCount.java
@@ -1,32 +1,44 @@
 package co.edu.uptc.logic;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
 
 public class ThreadCount implements Runnable {
 
-    private JTextField text;//Hay que cambiarlo para que acepte imagenes
+    private JLabel imagesSpace;//Hay que cambiarlo para que acepte imagenes
     private boolean state;
 
     private boolean sw;
 
-    public ThreadCount(JTextField text, boolean sw){
-        this.text = text;
+    public ThreadCount(JLabel imagesSpace, boolean sw){
+        this.imagesSpace = imagesSpace;
         this.sw = sw;
         state = true;
     }
 
     public void run(){
         if (sw){
-            this.minToMax();
+            try {
+                showImages();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            //this.minToMax();
         }else{
-            this.maxToMin();
+            //this.maxToMin();
         }
     }
 
+    private void showImages() throws IOException {
+        imagesSpace.setIcon(new ImageIcon(this.chargeFirstImage()));
+    }
+
     /*
-     *Muestra los numeros de menor a mayor
-     */
     private void minToMax(){
         for (var x = Integer.MIN_VALUE ; x <= Integer.MAX_VALUE && state ; x++){
             text.setText(""+x);//Va mostrando cada numero en pantalla
@@ -38,9 +50,7 @@ public class ThreadCount implements Runnable {
         }
     }
 
-    /*
-     *Muestra los numeros de mayor a menor
-     */
+
     private void maxToMin(){
         for (var x = Integer.MAX_VALUE ; x >= Integer.MIN_VALUE && state; x--){
             text.setText(""+x);
@@ -51,8 +61,31 @@ public class ThreadCount implements Runnable {
             }
         }
     }
-
+     */
     public void stopThread() {
         state = false;
+    }
+
+    private BufferedImage chargeFirstImage() throws IOException {
+        File imagenArchivo = new File("/TallerHilosSegundo50/src/imageResources/campan.jpg");
+        if (imagenArchivo.exists()) {
+            System.out.println("La ruta es válida y el archivo existe.");
+        } else {
+            System.out.println("La ruta no es válida o el archivo no existe.");
+        }
+        BufferedImage imagen = ImageIO.read(imagenArchivo);
+        return imagen;
+    }
+
+    private Image chargeSecondImage() throws IOException {
+        File imagenArchivo = new File("/TallerHilosSegundo50/src/imageResources/cerezas.png");
+        Image imagen = ImageIO.read(imagenArchivo);
+        return imagen;
+    }
+
+    private BufferedImage chargeThirdImage() throws IOException {
+        File imagenArchivo = new File("imageResources/diamante.png");
+        BufferedImage imagen = ImageIO.read(imagenArchivo);
+        return imagen;
     }
 }
